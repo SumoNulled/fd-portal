b2eeb4ac36af89321d8a1f955e58d7d262a2a5b6
Klay Bellator <sumo@null.net>
2023-07-05 16:34:36 -0500

    Set up Model and ViewModel

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,7 @@ namespace FDPortal
     /// </summary>
     public partial class App : Application
     {
+
+
     }
 }
