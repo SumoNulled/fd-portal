3cbecda484a68f2d85b4cf450cc1e8361bb4716d
Klay Bellator <sumo@null.net>
2023-07-05 07:25:24 -0500

    Added info-cards and title to main view.

---
+++ View/UserControls/Card.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FDPortal.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Card.xaml
+    /// </summary>
+    public partial class Card : UserControl
+    {
+        public Card()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(Card));
+
+        public string Number
+        {
+            get { return (string)GetValue(NumberProperty); }
+            set { SetValue(NumberProperty, value); }
+        }
+
+        public static readonly DependencyProperty NumberProperty =
+            DependencyProperty.Register("Number", typeof(string), typeof(Card));
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register("IsActive", typeof(bool), typeof(Card));
+
+        public bool IsTechnical
+        {
+            get { return (bool)GetValue(IsTechnicalProperty); }
+            set { SetValue(IsTechnicalProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsTechnicalProperty =
+            DependencyProperty.Register("IsTechnical", typeof(bool), typeof(Card));
+    }
+}
