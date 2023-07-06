5589fe049444628b4a8ec5590bbe98edf72ed56e
Klay Bellator <sumo@null.net>
2023-07-06 08:18:46 -0500

    Added view model base, relay command view model, and login view model.

--- ViewModel/ViewModelBase.cs
+++ ViewModel/ViewModelBase.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace FDPortal.ViewModel
 {
-    class ViewModelBase
+    public abstract class ViewModelBas : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
